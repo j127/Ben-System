7442755423b9c88006754514e12ef7734376dd72
J. Cohen <hostelmarketing@gmail.com>
2016-09-30 14:20:01 -0700

    Move systems to settings.py

---
+++ settings.py
@@ -0,0 +1,38 @@
+"""These settings can be extended to customize the system."""
+
+
+systems = {}
+
+systems['ben_pridmore'] = {
+    # TODO: double-check accuracy
+    'first_letters': ['s', 't', 'n', 'm', 'r', 'l', 'g/j', 'k', 'f/th', 'b',
+                      'p', 'h', 'sk/sn/sm', 'st/sp', 'sh/sl/sw', 'd', None,
+                      None, None],
+    'middle_letters': ['oo', 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O',
+                       None, None, None, None, None, None, 'ow', 'or', 'ar'],
+    'final_letters': ['s', 't', 'n', 'm', 'r', 'l', 'g', 'k', 'f/th', 'b',
+                      None, None, None, None, None, None, 'j/sh/ch', 'p', 'd'],
+    'decimals': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', None, None,
+                 None, None, None, None, None, None, None],
+    'binaries': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+                 '12', '13', '14', '15', None, None, None],
+    'card_suits': ['ss', 'cd', 'ch', 'cs', 'dc', 'dh', 'ds', 'cc', 'hc', 'hd',
+                   'hs', 'hh', 'sc', 'sd', 'sh', 'dd', None, None, None],
+    'card_values': ['10', 'A', '2', '3', '4', '5', '6', '7', '8', '9', None,
+                    None, None, None, None, None, 'J', 'Q', 'K'],
+}
+
+systems['josh_cohen'] = {
+    # TODO: fix this
+    'first_letters': [],
+    'middle_letters': [],
+    'final_letters': [],
+    'decimals': ['0',  '1', '2', '3', '4', '5', '6', '7', '8', '9', None, None,
+                 None, None, None, None, None, None, None],
+    'binaries': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+                 '12', '13', '14', '15', None, None, None],
+    'card_suits': [],
+    'card_values': ['10', 'A', '2', '3', '4', '5', '6', '7', '8', '9', None,
+                    None, None, None, None, None, 'J', 'Q', 'K'],
+}
+
